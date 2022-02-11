e8cc7d08e1f14d092540ce6bd5ea0cd33deebf1f
kysteno4ek <73881712+kysteno4ek@users.noreply.github.com>
2022-02-11 16:47:04 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+﻿
+#BOT:
+TOKEN = "5113183084:AAFtmWLwVLJivMIxCYUb-YWCofuubd6jdPw" # ТОКЕН БОТА
+ADMIN = 840605309 #ИД АДМИНА, узнать свой айди - @get_any_telegram_id_bot
+
+DIR = "" # Путь к файлу, если нужно
+
+#USER: # https://my.telegram.org/auth
+API_ID = 9143824
+API_HASH = "f9da0b7d5d5ad1db79c38267da3cfb5d"
+
+
---
+++ user.py
@@ -0,0 +1,39 @@
+from pyrogram import Client
+import config
+import asyncio
+from main import bot
+
+client = Client("chats", config.API_ID, config.API_HASH)
+client.start()
+
+async def get_chats():
+    list = []
+    async for dialog in client.iter_dialogs():
+        if dialog.chat.type == 'supergroup':
+            list.append({'title' : dialog.chat.first_name or dialog.chat.title, 'id' : dialog.chat.id})
+    return list
+
+async def leave_from_channel(id):
+    try:
+        await client.leave_chat(id)
+        return True
+    except:
+        return False
+
+async def spamming(spam_list, settings, db):
+    while settings[4] == 1:
+        for chat in spam_list:
+            settings = db.settings()
+            try:
+                with open(f'{config.DIR}{settings[1]}', 'rb') as photo:
+                    await client.send_photo(chat['id'], photo, caption=f"{settings[2]}\n\n{chat['text']}")
+                    await bot.send_message(config.ADMIN, f'✅ Cообщение в {chat["title"]} было успешно отправленно.')
+            except Exception as e:
+                try:
+                    await client.send_message(chat['id'], f"{settings[2]}\n\n{chat['text']}")
+                    await bot.send_message(config.ADMIN, f'✅ Cообщение в {chat["title"]} было успешно отправленно.')
+                except Exception as e:
+                    await bot.send_message(config.ADMIN, f'❌ Cообщение в {chat["title"]} не было отправлено из-за ошибки: {e}')
+            await asyncio.sleep(settings[5]*60)
+            if settings[4] != 1:
+                break
